12e70ab31a60ef9a32317a217a37214152f3cb3f
JenniferZh90 <jenniferzhou.zyj@gmail.com>
2023-02-16 22:12:17 +1100

    update error code

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Lots of details are ignored just to focus on making function work
 
 Below are examples of how to call the api after server is loaded
 1. book:
+```
 post http://localhost:8080/api/book with json body as
 {
     "user": "Jen",
@@ -12,29 +13,35 @@ post http://localhost:8080/api/book with json body as
     "start":"2023-01-03",
     "end":"2023-01-03"
 }
+```
 
 2. query
+```
 get http://localhost:8080/api/query?id=306022df37fb44a48f8bcfac75b6b161
 or 
 get http://localhost:8080/api/query?user=Jen
+```
 
 3. pickup
+```
 put http://localhost:8080/api/pickup with json body as
 {
     "id": "221b69c4b9794507a4ed9ed77476ff0a",
     "pickup": "2023-01-03"
 }
-
+```
 
 4. return
+```
 put http://localhost:8080/api/return with json body as
 {
     "id": "af6a7b45a0c3464f882c46f8b00e421f",
     "return": "2023-01-03"
 }
+```
 
 ## Installation
-Install elixir on mac with command `homebrew install elixir`
+Install elixir on mac with homebrew command `brew install elixir`
 
 ## Download library
 Download project lib with command `mix deps.get`
@@ -42,5 +49,15 @@ Download project lib with command `mix deps.get`
 ## Run project
 Run the project with command `mix run --no-halt`
 
+## If you want to access the running project with other shell locally
+1)Start the project with command like `elixir --sname cardeal -S mix run --no-halt`
+2)Open another shell and type command `iex --sname jen --remsh cardeal@<YourHost>`
+  e.g. `iex --sname jen --remsh cardeal@JennifersBook`
+3)In the newly opened shell, you can inspect gen_server state with 
+```
+iex(cardeal@JennifersBook)1> pid = Process.whereis(CardealApi)
+  #PID<0.444.0>
+iex(cardeal@JennifersBook)2> :sys.get_state(pid)
+```
 
 
--- lib/cardeal_api/router.ex
+++ lib/cardeal_api/router.ex
@@ -3,6 +3,16 @@ defmodule CardealApi.Router do
   use Plug.Router
   alias CardealApi.RouteHandler
 
+  @error_code %{
+    "booking_not_found" => 404,
+    "already_picked" => 400,
+    "pickup_before_start_date_or_after_ending_date" => 400,
+    "return_after_ending_date" => 400,
+    "car_not_found" => 404,
+    "booking_state_error" => 500,
+    "server_error" => 500
+  }
+
   plug(Plug.Logger)
   plug(Plug.Parsers,
     parsers: [:urlencoded, :json],
@@ -21,34 +31,35 @@ defmodule CardealApi.Router do
     with {:ok, data} <- RouteHandler.booking(conn) do
       send_resp(conn, 200, JSON.encode!(data))
     else
-      {:error, error} -> send_resp(conn, 404, error)
+      {:error, error} -> send_resp(conn, @error_code[error] || 404, error)
     end
   end
   get "/api/query" do
     with {:ok, data} <- RouteHandler.query_booking(conn) do
       send_resp(conn, 200, JSON.encode!(data))
     else
-      {:error, error} -> send_resp(conn, 404, error)
+      {:error, error} -> send_resp(conn, @error_code[error] || 404, error)
     end
   end
   put "/api/pickup" do
     with {:ok, data} <- RouteHandler.register_pickup(conn) do
       send_resp(conn, 200, JSON.encode!(data))
     else
-      {:error, error} -> send_resp(conn, 404, error)
+      {:error, error} ->
+        send_resp(conn, @error_code[error] || 404, error)
     end
   end
   put "/api/return" do
     with {:ok, data} <- RouteHandler.return(conn) do
       send_resp(conn, 200, JSON.encode!(data))
     else
-      {:error, error} -> send_resp(conn, 404, error)
+      {:error, error} -> send_resp(conn, @error_code[error] || 404, error)
     end
   end
 
   # Fallback handler when there was no match
   match _ do
-    send_resp(conn, 404, "Not Found")
+    send_resp(conn, 404, "API Not Found")
   end
 
 end
